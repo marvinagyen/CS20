7c6c9022282dc8a2e75fc54cd4b915422805ca49
marvinagyen <marvinagyeni3@gmail.com>
2021-11-21 20:36:12 -0500

    final submission, incomplete

--- jade_delight.php
+++ jade_delight.php
@@ -7,12 +7,17 @@
 
 <body>
   <?php
-
-  //establish connection info
+  // establish connection info
   $server = "sql306.epizy.com";
   $userid = "epiz_29671562";
   $pw = "WoquVwkBPcXD2";
   $db= "epiz_29671562_jadedelight";
+
+  // // establish connection info
+  // $server = "localhost";
+  // $userid = "";
+  // $pw = "";
+  // // $db= "epiz_29671562_jadedelight";
 
   // Create connection
   $conn = new mysqli($server, $userid, $pw );
@@ -22,27 +27,41 @@
   }
   echo "Connected successfully";
 
-  // //select the database
-  // $conn->select_db($db);
-  //
-  // //run a query
-  // $sql = "SELECT * FROM pets";
-  // $result = $conn->query($sql);
-  // //get results
-  // if ($result->num_rows > 0)
-  // {
-  //     while($row = $result->fetch_assoc())
-  //    {
-  //      echo $row["name"]. " " . $row["type"]. "<br>";
-  //      }
-  // }
-  // else
-  //   echo "no results";
+  //select the database
+  $conn->select_db($db);
+
+  //run a query
+  $sql = "SELECT * FROM itemName";
+  echo "<br />The query is: " . $sql ."<br />";
+  $result = $conn->query($sql);
+  //get results
+  if ($result->num_rows > 0)
+  {
+      while($row = $result->fetch_assoc())
+     {
+       echo $row["name"]. " " . $row["type"]. "<br>";
+       }
+  }
+  else
+    echo "no results";
 
 //close the connection
 $conn->close()
 
+// $s = ""
+// for ($i = 0; $i <= 10; $i++) {
+//   $s += "<tr><td>";
+//   $s += makeSelect("quan" + $i, 0, 10);
+//   $s += "</td><td>" + menuItems[$i].name + "</td>";
+//   $s += "<td> $ " + menuItems[$i].cost.toFixed(2) + "</td>";
+//   $s += "<td>$<input type='text' name='cost'/></td></tr>";
+// }
+
   ?>
+
+<script language="javascript">
+
+</script>
 
 </body>
 </html>
